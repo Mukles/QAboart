7cb60b639351d9296013d37892f66f614c1218fa
Mukles <Mukleshossen1763@gmail.com>
2022-03-06 17:44:27 +0600

    home page done for mobile devices

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      width:{
+        "90": "22.75rem"
+      },
+      colors: {
+        "QAboat-blue": "#016DB3",
+        "QAboat-black": '#071F39'
+
+      },
+      
+      fontFamily:{
+        Comfortaa: ["Comfortaa"],
+        Poppins:["Poppins"]
+      },
+      backgroundPosition{
+        "left-70": "70% 0",
+      },
+    },
+  },
+  plugins: [],
+}
